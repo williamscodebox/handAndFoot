6d4d168c1fb111f09792c7b9e43f7afad314b809
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-05-04 17:23:21 -0400

    updated logic on counters

--- app/index.tsx
+++ app/index.tsx
@@ -1,6 +1,6 @@
 import AppButton from "@/components/AppButton";
 import SingleCardCount from "@/components/SingleCardCount";
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import {
   Alert,
   FlatList,
@@ -12,30 +12,26 @@ import {
 
 export default function Index() {
   const [counters, setCounters] = useState([
-    { id: "1", name: "Aces (20 Points)", value: 0 },
-    { id: "2", name: "Kings (10 Points)", value: 0 },
-    { id: "3", name: "Queens (10 Points)", value: 0 },
-    { id: "4", name: "Jacks (10 Points)", value: 0 },
-    { id: "5", name: "Kings (10 Points)", value: 0 },
-    { id: "6", name: "Queens (10 Points)", value: 0 },
-    { id: "7", name: "Queens (10 Points)", value: 0 },
-    { id: "8", name: "Jacks (10 Points)", value: 0 },
-    { id: "9", name: "Tens (10 Points)", value: 0 },
-    { id: "10", name: "Nines (10 Points)", value: 0 },
-    { id: "11", name: "Eights (10 Points)", value: 0 },
-    { id: "12", name: "Sevens (5 Points)", value: 0 },
-    { id: "13", name: "Sixes (5 Points)", value: 0 },
-    { id: "14", name: "Fives (5 Points)", value: 0 },
-    { id: "15", name: "Fours (5 Points)", value: 0 },
-    { id: "16", name: "Black Threes (0 Points)", value: 0 },
-    { id: "17", name: "Red Threes (-500 Points)", value: 0 },
-    { id: "18", name: "Twos (20 Points)", value: 0 },
-    { id: "19", name: "Jokers (50 Points)", value: 0 },
+    { id: "1", name: "Aces (20 Points)", value: 0, pointValue: 20 },
+    { id: "2", name: "Kings (10 Points)", value: 0, pointValue: 10 },
+    { id: "3", name: "Queens (10 Points)", value: 0, pointValue: 10 },
+    { id: "4", name: "Jacks (10 Points)", value: 0, pointValue: 10 },
+    { id: "5", name: "Tens (10 Points)", value: 0, pointValue: 10 },
+    { id: "6", name: "Nines (10 Points)", value: 0, pointValue: 10 },
+    { id: "7", name: "Eights (10 Points)", value: 0, pointValue: 10 },
+    { id: "8", name: "Sevens (5 Points)", value: 0, pointValue: 5 },
+    { id: "9", name: "Sixes (5 Points)", value: 0, pointValue: 5 },
+    { id: "10", name: "Fives (5 Points)", value: 0, pointValue: 5 },
+    { id: "11", name: "Fours (5 Points)", value: 0, pointValue: 5 },
+    { id: "12", name: "Black Threes (0 Points)", value: 0, pointValue: 0 },
+    { id: "13", name: "Red Threes (-500 Points)", value: 0, pointValue: -500 },
+    { id: "14", name: "Twos (20 Points)", value: 0, pointValue: 20 },
+    { id: "15", name: "Jokers (50 Points)", value: 0, pointValue: 50 },
   ]);
 
-  useEffect(() => {
-    console.log("Counters updated:", counters);
-  }, [counters]);
+  // useEffect(() => {
+
+  // }, [counters]);
 
   const updateCombinedState = (id, newValue) => {
     setCounters((prevCounters) =>
@@ -45,7 +41,16 @@ export default function Index() {
     );
   };
 
-  const totalValue = counters.reduce((acc, counter) => acc + counter.value, 0);
+  const clearAllCounters = () => {
+    setCounters((prevCounters) =>
+      prevCounters.map((counter) => ({ ...counter, value: 0 }))
+    );
+  };
+
+  const totalValue = counters.reduce(
+    (acc, counter) => acc + counter.value * counter.pointValue,
+    0
+  );
 
   // Define the renderItem function
   const renderItem = ({ item }) => (
@@ -60,6 +65,7 @@ export default function Index() {
   );
   const runPrompt = () => {
     Alert.alert("Score Cleared", "All scores have been cleared.");
+    clearAllCounters();
   };
 
   return (
