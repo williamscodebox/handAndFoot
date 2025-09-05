1928052e933c1a57952a48f1501332a44bd7422e
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-09-05 09:23:45 -0400

    added new index

---
+++ app2/index.js
@@ -0,0 +1,11 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+        Welcome to Hand & Foot
+      </Text>
+    </View>
+  );
+}
