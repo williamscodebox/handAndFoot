97f9cca2ca0a3afad9c4c64278fa05d22e4283da
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-09-05 09:31:04 -0400

    updated new index

--- app2/index.js
+++ app2/index.js
@@ -1,11 +1,34 @@
-import { Text, View } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
+import { Text } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+    <LinearGradient colors={["#fef2f2", "#eff6ff"]} style={styles.container}>
       <Text style={{ fontSize: 24, fontWeight: "bold" }}>
         Welcome to Hand & Foot
       </Text>
-    </View>
+    </LinearGradient>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "#1f2937", // gray-800
+  },
+});
+
+// <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+{
+  /* / may have to remove view component with the gradient setup */
+}
+// </View>
+
+// wrap code with this view if gradient and styles not working
