7a84610e279da137d0d2725e5528efc91cf688f0
williamscodebox <138732551+williamscodebox@users.noreply.github.com>
2025-09-10 16:42:20 -0400

    updated history page

--- app/history.js
+++ app/history.js
@@ -1,9 +1,38 @@
-import { Text, View } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function history() {
+  const navigation = useNavigation();
   return (
-    <View>
-      <Text>history</Text>
+    <View style={styles.container}>
+      <TouchableOpacity
+        style={styles.testButton}
+        onPress={() => {
+          console.log("✅ Button pressed!");
+          navigation.navigate("newgame");
+        }}
+      >
+        <Text style={styles.testText}>Test Press</Text>
+      </TouchableOpacity>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  testButton: {
+    backgroundColor: "tomato",
+    paddingVertical: 16,
+    paddingHorizontal: 32,
+    borderRadius: 8,
+  },
+  testText: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+});
